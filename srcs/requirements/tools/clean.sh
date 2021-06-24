4c374bb92c3df6d02d89337f3cdced48d9993535
clorin <cyrille.lorin@yahoo.fr>
2021-06-24 13:52:15 +0200

    FIX: remove only PATH_LOGIN/data

--- srcs/requirements/tools/clean.sh
+++ srcs/requirements/tools/clean.sh
@@ -17,6 +17,6 @@ if [[ $(cat /etc/hosts | grep "127.0.0.1 ${DOMAIN_NAME}" | wc -l) -eq 1 ]] ; the
 	sudo sed -i "/127.0.0.1 $DOMAIN_NAME/d" /etc/hosts
 fi
 
-echo -n "remove data's folder ${PATH_LOGIN}..."
-sudo rm -rf ${PATH_LOGIN}
+echo -n "remove data's folder ${PATH_LOGIN}/data..."
+sudo rm -rf ${PATH_LOGIN}/data
 echo "ok"
