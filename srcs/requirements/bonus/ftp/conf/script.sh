610a6fbd92598317166b7b19a0f4c15c61fe0c31
clorin <clorin@student.42.fr>
2021-06-25 10:37:48 +0200

    ADD: Bonus ftp

--- srcs/requirements/bonus/ftp/conf/script.sh
+++ srcs/requirements/bonus/ftp/conf/script.sh
@@ -12,10 +12,25 @@
 
 #!/bin/sh
 
+path=$(echo ${PATH_FTP} | sed 's_/_\\/_g')
+sed -i "s/PATH_FTP/${path}/g" vsftpd.conf
+
+cert=$(echo ${CERT_KEY} | sed 's_/_\\/_g')
+sed -i "s/CERT_KEY/${cert}/g" vsftpd.conf
+
 cp vsftpd.conf /etc/vsftpd
 
 { echo "user42"; echo "user42"; } | adduser clorinftp
 
 cp vsftpd.user_list /etc/vsftpd
+
+if [ ! -f ${CERT_KEY} ] ; then
+	echo -n "Waiting for certificat "
+	while [ ! -f ${CERT_KEY} ]; do
+		echo -n "."
+		sleep 1
+	done
+	echo "ok"
+fi
 
 vsftpd /etc/vsftpd/vsftpd.conf
