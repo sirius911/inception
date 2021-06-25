6bd3b5551004b3552f5731812e0c922e42088620
clorin <cyrille.lorin@yahoo.fr>
2021-06-25 15:15:01 +0200

    ADD: userftp in .env

--- srcs/requirements/bonus/ftp/conf/script.sh
+++ srcs/requirements/bonus/ftp/conf/script.sh
@@ -12,6 +12,8 @@
 
 #!/bin/sh
 
+sed -i "s/FTP_USER/${FTP_USER}/g" vsftpd.conf
+
 path=$(echo ${PATH_FTP} | sed 's_/_\\/_g')
 sed -i "s/PATH_FTP/${path}/g" vsftpd.conf
 
@@ -20,7 +22,9 @@ sed -i "s/CERTS_KEY/${cert}/g" vsftpd.conf
 
 cp vsftpd.conf /etc/vsftpd
 
-{ echo "user42"; echo "user42"; } | adduser clorin
+{ echo "$FTP_PASSWORD"; echo "$FTP_PASSWORD"; } | adduser ${FTP_USER}
+
+sed -i "s/FTP_USER/${FTP_USER}/g" vsftpd.user_list
 
 cp vsftpd.user_list /etc/vsftpd
 
